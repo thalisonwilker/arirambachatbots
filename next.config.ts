aff9bcdac86210d347f50de128d9d3551142795a
Thalyson <thalison.wilker@gmail.com>
2026-05-12 01:01:10 -0300

    ci: update build command and enable base path configuration for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,8 @@ const nextConfig: NextConfig = {
   },
   // Se for fazer deploy para <username>.github.io/<repository-name>/
   // Descomente as linhas abaixo e substitua pelo nome do seu repositório:
-  // basePath: isGithubActions ? '/arirambachatbots' : '',
-  // assetPrefix: isGithubActions ? '/arirambachatbots/' : '',
+  basePath: isGithubActions ? '/arirambachatbots' : '',
+  assetPrefix: isGithubActions ? '/arirambachatbots/' : '',
 };
 
 export default nextConfig;
