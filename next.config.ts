5d90f49d8ed690da566f8be41be1d5e653de8686
Thalyson <thalison.wilker@gmail.com>
2026-05-11 23:55:43 -0300

    feat: add GitHub Pages deployment workflow and configure static export for Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Se for fazer deploy para <username>.github.io/<repository-name>/
+  // Descomente as linhas abaixo e substitua pelo nome do seu repositório:
+  // basePath: isGithubActions ? '/arirambachatbots' : '',
+  // assetPrefix: isGithubActions ? '/arirambachatbots/' : '',
 };
 
 export default nextConfig;
